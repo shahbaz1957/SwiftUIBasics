20a835166e3f7e61f856886a11834838bf930e15
Shahbaz Alam <shahbazjamia1957@gmail.com>
2025-12-13 16:04:06 +0530

    add shapes

---
+++ SwiftUIBasics/ShapesView.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesView.swift
+//  SwiftUIBasics
+//
+//  Created by SHAHBAZ ALAM on 13/12/25.
+//
+
+import SwiftUI
+
+struct ShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesView()
+}
